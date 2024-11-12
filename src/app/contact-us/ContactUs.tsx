4381e5e1ce36ea5ac0acd798465c976624a16347
jishnum007 <jishnu@globalsurf.ae>
2024-11-12 11:27:58 +0530

    contact us

--- src/app/contact-us/ContactUs.tsx
+++ src/app/contact-us/ContactUs.tsx
@@ -5,7 +5,7 @@ export default function Contact() {
   return (
     <Fragment>
          <main className="content-wrapper">
-        <BannerVideo title="Contact" videoSrc="/media/contact-us.webm"  posterSrc="media/contact-us01.webp" ></BannerVideo>
+        <BannerVideo title="Contact us" videoSrc="/media/contact-us.webm"  posterSrc="media/contact-us01.webp" ></BannerVideo>
         <ContactForm/>
         </main>
     </Fragment>
