5fdd28959005d5958d460911543ea12876bfd595
nived <nivuyt789@gmail.com>
2025-03-27 16:16:23 +0530

    fix:email change

--- src/app/api/send-mail/route.ts
+++ src/app/api/send-mail/route.ts
@@ -18,7 +18,8 @@ export async function POST(req: NextRequest) {
 
         const { data, error } = await resend.emails.send({
             from: `City Marine <enquiry@citymarinebrokers.com>`,
-            to: ['info@citymarinebrokers.com'],
+            to: ['khadar@Citymarinebrokers.com'],
+            cc: ['harsha@globalsurf.ae'],
             subject: 'Enquiry from website [citymarinebrokers.com]',
             react: EmailTemplate({ name, phone, email, message }),
             replyTo: email,
